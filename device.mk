ef98b8d160dee0274de7c89d15429de8a90e3231
Flopster101 <nahuelgomez329@gmail.com>
2025-09-10 13:46:42 -0300

    m34x: Add device tree to Soong namespace
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- device.mk
+++ device.mk
@@ -61,3 +61,7 @@ PRODUCT_COPY_FILES += \
     $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/os.checked.bin \
     $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/NPU.bin \
     $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/vts.bin
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH) \
