1475d921a7933e9525e0172b7758aa588271e6b6
Majaahh <maja.blaszczak@proton.me>
2025-10-10 11:13:41 +0200

    m34x: Move firmware placeholders to vendor tree
    Change-Id: I926a75934babba36c717e34914df485ea9ff7c5a
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -47,15 +47,5 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
-# Placeholders
-EMPTY_PLACEHOLDER := $(DEVICE_PATH)/configs/placeholder
-
-PRODUCT_COPY_FILES += \
-    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram.bin \
-    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/mfc_fw.bin \
-    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/os.checked.bin \
-    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/NPU.bin \
-    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/vts.bin
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
