2c2b53828abe69d758ab7ac75bf39d9fd333cff2
Majaahh <maja.blaszczak@proton.me>
2025-10-26 15:42:32 +0100

    m34x: Commonize keymint blobs
    Change-Id: I5788612a61ef3b9447ba89882651d00976d16341
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -40,21 +40,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libexynoscamera3.so': blob_fixup()
         .add_needed('libshim_camera.so')
         .binary_regex_replace(b'_ZN7android5Fence', b'_ZN7exynos55Fence'),
-    # Keymint
-    (
-        'vendor/bin/hw/android.hardware.security.keymint-service.samsung',
-        'vendor/lib64/libskeymint10device.so',
-        'vendor/lib64/libskeymint_cli.so',
-    ): blob_fixup()
-        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
-            'android.hardware.security.keymint-V4-ndk.so')
-        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
-            'android.hardware.security.secureclock-V1-ndk.so')
-        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
-             'android.hardware.security.sharedsecret-V1-ndk.so')
-        .add_needed('android.hardware.security.rkp-V3-ndk.so')
-        .replace_needed('libcrypto.so', 'libcrypto-tm.so')
-        .add_needed('libshim_crypto.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
