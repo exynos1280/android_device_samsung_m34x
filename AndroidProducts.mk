fb8f6d37271ceb878f5e1d1600adf2e326c532e7
Majaahh <maja.blaszczak@proton.me>
2025-11-07 09:38:27 +0100

    m34x: Adapt to PixelOS
    Change-Id: If8b11aa88c2cf099b96c5dffb6229a999df2fbdf
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) The LineageOS Project
+# Copyright (C) The PixelOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m34x.mk
+    $(LOCAL_DIR)/custom_m34x.mk
